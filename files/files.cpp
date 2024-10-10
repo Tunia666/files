c939f4b7918973ba1da4d1644df768757b1ab0ca
truda <truda1999@gmail.com>
2024-10-10 09:15:37 +0300

    Добавьте файлы проекта.

---
+++ files/files.cpp
@@ -0,0 +1,41 @@
+﻿#include <fstream>
+#include <iostream>
+
+int main()
+{/*
+    ifstream in;//входной покот 
+    ofstream out;//выходной поток
+    fstream both;//двунаправленный поток
+    
+    in.open(test.txt);// создать поток с помощью open
+    ifstream in (test.txt);// открыть входной поток in и связать его с файлом
+    //пример кода на ошибку открытия потока
+    if (!in){
+        cout <<error;
+        return -1;
+    }
+    in >>x;//считывается значение из потока in в переменную x
+    out <<x;//помещаем значение переменной x d 
+    //
+    double next, sum = 0;
+    int count = 0;
+    while (in>>next)
+    {
+        sum += next;
+        count++;
+    }
+    double sr = sum/count;
+    
+    in.close();
+    out.close();
+    ifstream in("infile.dat", los::binary);
+    in.get(ch); //считывание 1 символа и помещение его в переменную
+    in.put(ch); //запись в символ и возврат ссылки на поток
+    if (!in.eof()) cout << "not endl";
+    else cout << "yes endl";
+    while (in.peek() != EOF) {
+        in.get(next);
+        cout << next;
+    }
+    return 0;*/
+}
